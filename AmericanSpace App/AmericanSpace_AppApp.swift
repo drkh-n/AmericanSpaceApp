2c59a0d36fe91d1b9104257c95c97d3aa8fda13b
drkh-n <darkhan.nur12@gmail.com>
2021-04-26 19:40:28 +0600

    Initial Commit

---
+++ AmericanSpace App/AmericanSpace_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericanSpace_AppApp.swift
+//  AmericanSpace App
+//
+//  Created by Darkhan on 26.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AmericanSpace_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmericanSpace App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmericanSpace App
+//
+//  Created by Darkhan on 26.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
